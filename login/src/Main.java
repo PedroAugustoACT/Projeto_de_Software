d018b437016ae893c38326aa799e8e500657423e
PedroAugustoACT <pedro.augusto.dr11@gmail.com>
2022-07-06 09:28:56 -0300

    Command: delete swicht and souts, add if return

--- login/src/Main.java
+++ login/src/Main.java
@@ -12,134 +12,16 @@ public class Main {
             for (CommandMenuParent command : invoker.commands) {
                System.out.println("Pressione " + command.number + " para: " + command.name);
              }
-            // System.out.println("Pressione 1 para: Editar perfil");
-            // System.out.println("Pressione 2 para: Adicionar amigo");
-            // System.out.println("Pressione 3 para: Ver convites");
-            // System.out.println("Pressione 4 para: Enviar mensagem");
-            // System.out.println("Pressione 5 para: Criar comunidade");
-            // System.out.println("Pressione 6 para: Entrar em uma comunidade");
-            // System.out.println("Pressione 7 para: Listas minhas informações");
-            // System.out.println("Pressione 8 para: Remover conta");
-            // System.out.println("Pressione 9 para: Enviar mensagem no feed");
-            // System.out.println("Pressione 10 para: Ver lista de amigos");
-            // System.out.println("Pressione 11 para: Ver mensagens diretas");
-            // System.out.println("Pressione 12 para: Ver mensagens do feed");
-            // System.out.println("Pressione 13 para: Logout");
             System.out.print("=> ");
 
             try {
                 int value = myObj.nextInt();
                 invoker.executeCommand(value, myObj, account, editable);
-                // switch (value) {
-
-                //     case 1:
-                //         System.out.println("EDITAR/CRIAR ATRIBUTO");
-
-                //         try {
-                //             editable.managerPerfil(myObj, account);
-                //         } catch (InputMismatchException e) {
-                //             System.out.println("DIGITE UM NÚMERO VÁLIDO");
-                //         } catch (ExceptionAtribute nullAtribute) {
-                //             System.out.println(nullAtribute.getMessage());
-                //         } finally {
-                //             myObj.nextLine();
-                //         }
-
-                //         break;
-
-                //     case 2:
-                //         System.out.println("ADICIONAR AMIGO");
-                //         try {
-                //             editable.sendInvite(myObj, account);
-                //         }
-                //         catch(ExceptionLogin nullLogin){
-                //             System.out.println(nullLogin.getMessage());
-                //         }
-                //         catch (Exception nullFriend) {
-                //             System.out.println(nullFriend.getMessage());
-                //         }
-                        
-
-                //         break;
-
-                //     case 3:
-                //         System.out.println("VER CONVITES");
-                //         try{
-                //             editable.answerInvite(myObj, account);
-                //             break;
-                //         }
-                //         catch(ExceptionLogin nullLogin){
-                //             System.out.println(nullLogin.getMessage());
-                //         }
-                //         break;
-
-                //     case 4:
-                //         System.out.println("ENVIAR MENSAGEM");
-                //         try {
-                //             editable.sendMessage(myObj, account);
-                            
-                //         } catch (ExceptionLogin nullLogin) {
-                //             System.out.println(nullLogin.getMessage());
-                //         }
-                //         break;
-
-                //     case 5:
-
-                //         System.out.println("CRIAR COMUNIDADE");
-
-                //         editable.addComunity(myObj, account);
-                //         break;
-
-                //     case 6:
-
-                //         System.out.println("Entrar em uma comunidade");
-                //         try {
-                //             editable.addMember(myObj, account);
-                //             break;
-                //         } catch (ExceptionComunity nullComunity) {
-                //             System.out.println(nullComunity.getMessage());
-                //         }
-                        
-
-                //     case 7:
-                //         System.out.println("Listar informações");
-
-                //         editable.listInformations(myObj, account);
-                //         break;
-
-                //     case 8:
-                //         System.out.println("Remover sua conta");
-
-                //         if (editable.removeAccount(myObj, account)) {
-                //             return;
-                //         }
-
-                //         break;
-
-                //     case 9:
-                //         System.out.println("Enviar mensagem no feed");
-                //         editable.sendFeed(myObj, account);
-                //         break;
-
-                //     case 10:
-                //         System.out.println(account.friends);
-                //         break;
-
-                //     case 11:
-                //         System.out.println(account.messages);
-                //         break;
-
-                //     case 12:
-
-                //         System.out.println(account.messagesFeed);
-                //         System.out.println(account.messagesFriends);
-                //         break;
-                //     case 13:
-                //         return;
-
-                //     default:
-                //         System.out.println("DIGITE UM NÚMERO VÁLIDO");
-                // }
+                if(value == 13){
+                    System.out.println("Pressione Enter novamente");
+                    return;
+                }
+                
             } catch (InputMismatchException e) {
                 System.out.println("DIGITE UM NÚMERO VÁLIDO");
             } finally {
